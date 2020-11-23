ddc3f24d3311a0feda2b9df5d81e526829a9136e
Antonio Desktop <aslan.suarez@gmail.com>
2020-11-23 19:13:06 +0100

    Thinkpad and desktop mode

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,8 +1,30 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Comentar y descomentar dependiendo si es thinkpad o no
+
+// Thinkpad
+
+/*
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
 	{"CPU:", "CPU_thinkpad",								5,		0},
+	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	15,		0},
+
+	{"", "date | awk '{print $1,$2,$3}'",				360,		0},
+	{"", "date | awk '{print $4,$5,$6}'",				1,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
+*/
+
+
+
+
+// Desktop
+
+static const Block blocks[] = {
+
+	{"CPU:", "CPU_desktop",								5,		0},
 	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	15,		0},
 
 	{"", "date | awk '{print $1,$2,$3}'",				360,		0},
